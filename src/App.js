5a2ead45fe14ce565d5ecfd2b0ab461e2930774e
paul-antony <paulantonytoc@gmail.com>
2022-06-25 18:06:51 +0530

    implemented nav bar comppnent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
+import Nav from './Nav/Nav';
 
 export default function App() {
   return (
   <div>
-<div class="navbar bg-primary text-primary-content">
 
-  <div class="flex-1">
-  <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-  </div>
-</div>
-    
+    <Nav></Nav>
     </div>
   )
 }
